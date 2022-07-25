4eb7c9f303eca81008979dd1216fcd8f6c7833ef
EliasGalindo0 <eliasgalindo@gmail.com>
2022-07-25 15:03:26 -0300

    fix: test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,12 +14,12 @@ const options = {
   database: 
     `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+  password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
-  logging: process.env.DEBUG !== 'false',
+  // logging: process.env.DEBUG !== 'false',
 };
 
 module.exports = {
